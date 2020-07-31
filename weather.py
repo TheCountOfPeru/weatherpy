44374c9d6cd7115b74f40df0e54522bcc8d09dc5
TheCountOfPeru <webber989@gmail.com>
2020-07-31 17:21:55 -0600

    update

--- weather.py
+++ weather.py
@@ -18,7 +18,7 @@ def argumentParserBuilder():
 def convertKelvinToCelcius(temperatureInKelvin):
     return temperatureInKelvin - kelvinConstant
 
-def queryWeatherFor(thisLocation):
+def queryWeatherAPIFor(thisLocation):
     parameters = {"q": thisLocation, "appid": APIKey}
     # Make a get request to get the latest position of the international space station from the opennotify api.
     response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=parameters)
@@ -29,14 +29,44 @@ def queryWeatherFor(thisLocation):
     # print(type(data))
     # print(convertKelvinToCelcius(weatherData["main"]["temp"]))
     return response.json()
+    
+def getWeatherDescription(weatherQueryResults): 
+    return weatherQueryResults["weather"][0]["description"]
 
+def getTemperature(weatherQueryResults):
+    return weatherQueryResults["main"]["temp"] - kelvinConstant
 
-def displayWeatherFor(weatherQueryResults):
+def getFeelsLikeTemperature(weatherQueryResults):
+    return weatherQueryResults["main"]["feels_like"] - kelvinConstant
 
+def getHumidity(weatherQueryResults):
+    return weatherQueryResults["main"]["humidity"]
 
+def getLocationName(weatherQueryResults):       
+    return weatherQueryResults["name"]
+
+def getLocationCountry(weatherQueryResults):         
+    return weatherQueryResults["sys"]["country"]
+
+def convertQueryResultToDict(weatherQueryResults):
+  
+    weatherInfomationDict = {
+        "description": getWeatherDescription(weatherQueryResults),
+        "temperature":getTemperature(weatherQueryResults),
+        "feels_like": getFeelsLikeTemperature(weatherQueryResults),
+        "humidity":getHumidity(weatherQueryResults),
+        "locationName":getLocationName(weatherQueryResults),
+        "locationCountry":getLocationCountry(weatherQueryResults),
+    }
+    return weatherInfomationDict
+
+def displayWeatherReport(weatherDictionary):
+    wd = weatherDictionary
+    print(wd["description"] + ", " + str(wd["temperature"]) + )
 
 if __name__ == "__main__":
     parser = argumentParserBuilder()
     args = parser.parse_args()
-    weatherQueryResults = queryWeatherFor(args.location)
-    
+    weatherQueryResults = queryWeatherAPIFor(args.location)
+    weatherDictionary = convertQueryResultToDict(weatherQueryResults)
+    displayWeatherReport(weatherDictionary)
